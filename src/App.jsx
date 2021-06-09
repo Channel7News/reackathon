d5d906170d91d058b7f626d729f49286d1b6085f
victorzottmann <zottmann.victor@gmail.com>
2021-06-10 09:56:00 +1000

    first commit, initial setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import './App.css'
+// import Header from './components/Header'
+// import Navbar from './components/Navbar'
+// import Filter from './components/Filter'
+// import ArticlesList from './components/ArticlesList'
+// import Article from './components/Article'
+// import Footer from './components/Footer'
+
+const App = () => {
+  const apiData = () => {
+    const apiKey = '324369bedbcf4ccbb3c61134b2d0da9d'
+    fetch(
+      `https://newsapi.org/v2/everything?q=apple&from=2021-06-08&to=2021-06-08&sortBy=popularity&apiKey=${apiKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+      </header>
+      {apiData()}
+    </div>
+  )
+}
+
+export default App
