f016aab219268a6257c65a1e2bd237afc3e2d914
whywesmurfing <raymond.doan94@gmail.com>
2021-06-10 16:49:35 +1000

    Added in comments

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,6 @@ const App = () => {
   const [articleData, setArticleData] = useState();
   const apiKey = "324369bedbcf4ccbb3c61134b2d0da9d";
 
-  const ApiData = () => {
-    fetch(
-      `https://newsapi.org/v2/everything?q=apple&from=2021-06-08&to=2021-06-08&sortBy=popularity&apiKey=${apiKey}`
-    )
-      .then((res) => res.json())
-      .then((data) => setArticleData(data));
-  };
-
   useEffect(() => {
     fetch(
       `https://newsapi.org/v2/everything?q=apple&from=2021-06-08&to=2021-06-08&sortBy=popularity&apiKey=${apiKey}`
@@ -35,11 +27,11 @@ const App = () => {
 
   // Setting the Country Code
   const [countryCode, setCountryCode] = useState("au");
-
   const handleCountryChange = (code) => {
     setCountryCode(code);
   };
 
+  // Pulling the Headlines data
   const headlinesData = () => {
     fetch(
       `https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=${apiKey}`
