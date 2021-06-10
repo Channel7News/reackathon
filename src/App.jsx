5e363668b4139985c6e70dad2af5f9bdfebca25a
whywesmurfing <raymond.doan94@gmail.com>
2021-06-10 16:32:16 +1000

    updated styling to fit articles below the header and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import ArticlesList from "./components/ArticlesList";
 
-import './App.css'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import CountrySelector from './components/CountrySelector'
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import CountrySelector from "./components/CountrySelector";
 // import Filter from './components/Filter'
 // import Footer from './components/Footer'
 
@@ -15,39 +15,40 @@ const App = () => {
   const ApiData = () => {
     fetch(
       `https://newsapi.org/v2/everything?q=apple&from=2021-06-08&to=2021-06-08&sortBy=popularity&apiKey=${apiKey}`
-    ).then((res) => res.json())
-    .then((data) => setArticleData(data));
-  }, []);
-
-  useEffect(() => {
-        fetch(
-      `https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=${apiKey}`
     )
       .then((res) => res.json())
-  }
+      .then((data) => setArticleData(data));
+  };
 
-  const [countryCode, setCountryCode] = useState('au')
+  useEffect(() => {
+    fetch(
+      `https://newsapi.org/v2/everything?q=apple&from=2021-06-08&to=2021-06-08&sortBy=popularity&apiKey=${apiKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => setArticleData(data));
+  }, []);
+
+  const [countryCode, setCountryCode] = useState("au");
 
   const handleCountryChange = (code) => {
-    setCountryCode(code)
-  }
+    setCountryCode(code);
+  };
 
   const headlinesData = () => {
     fetch(
       `https://newsapi.org/v2/top-headlines?country=${countryCode}&apiKey=${apiKey}`
     )
       .then((res) => res.json())
-      .then((data) => console.log('DATA:', data.articles))
-  }
-     
+      .then((data) => console.log("DATA:", data.articles));
+  };
 
   return (
-    <div>
+    <div class="App">
       <Header />
       <Navbar handleCountryChange={handleCountryChange} />
       <ArticlesList articleData={articleData} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/styles/ArticlesList.jsx
+++ src/styles/ArticlesList.jsx
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 export const ArticlesContainer = styled.div`
+  position: relative;
   margin: 20px;
   width: 95%;
-  height: 700vh;
+  height: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
